f1debe6d4e58352f0c28685136320f5ba19a2670
subhidh <subhidhagarwal@gmail.com>
2022-01-23 20:41:52 +0530

    validate status

--- src/main/java/basics.java
+++ src/main/java/basics.java
@@ -1,30 +1,44 @@
+import files.ReusableMethods;
 import files.payload;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class basics {
     public static void main(String[] x){
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        //add new place to maps
         String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
                 .body(payload.AddPlace())
                 .when().post("maps/api/place/add/json")
                 .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("Server", "Apache/2.4.18 (Ubuntu)").extract().asString();
-        
+
+        //store place ID
         JsonPath js=new JsonPath(response); //for parsing Json
         String place_id = js.getString("place_id");
         System.out.println(place_id);
 
+
+        String new_address = "Jharkhand, India";
+        //update place
         given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-                .body(payload.UpdatePlace(place_id))
+                .body(payload.UpdatePlace(place_id, new_address))
                 .when().put("/maps/api/place/update/json")
                 .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
 
-        given().log().all().queryParam("key", "qaclick123").queryParam("place_id",place_id)
-                .when().get("maps/api/place/add/json")
-                .then().assertThat().log().all().
+        //Check if new place added is correct or not
+        String getPlaceResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id",place_id)
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200).extract().response().asString();
+
+
+        String gotAddress = ReusableMethods.extractValuefromJson(getPlaceResponse, "address");
+
+        Assert.assertEquals(gotAddress, new_address);
     }
 }
 
