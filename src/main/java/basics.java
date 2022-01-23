0a68141c957f754b5bc6443c2e8703b8e126261b
subhidh <subhidhagarwal@gmail.com>
2022-01-23 19:45:05 +0530

    validate status

--- src/main/java/basics.java
+++ src/main/java/basics.java
@@ -1,29 +1,30 @@
+import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class basics {
     public static void main(String[] x){
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body("{\r\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Subhidh Agarwal\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09ww\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\r\n")
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(payload.AddPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-                .header("Server", "Apache/2.4.18 (Ubuntu)");
+                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.18 (Ubuntu)").extract().asString();
+        
+        JsonPath js=new JsonPath(response); //for parsing Json
+        String place_id = js.getString("place_id");
+        System.out.println(place_id);
+
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+                .body(payload.UpdatePlace(place_id))
+                .when().put("/maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        given().log().all().queryParam("key", "qaclick123").queryParam("place_id",place_id)
+                .when().get("maps/api/place/add/json")
+                .then().assertThat().log().all().
     }
 }
 
