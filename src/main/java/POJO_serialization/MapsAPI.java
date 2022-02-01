d089504c35d214926eacca342468c8c2d387a1fd
subhidh <subhidhagarwal@gmail.com>
2022-02-01 19:08:59 +0530

    SpecBuilders Update

--- src/main/java/POJO_serialization/MapsAPI.java
+++ src/main/java/POJO_serialization/MapsAPI.java
@@ -2,7 +2,12 @@ package POJO_serialization;
 
 import static io.restassured.RestAssured.*;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +32,17 @@ public class MapsAPI {
         add.setLanguage("English");
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        Response response = given().log().all().queryParam("key", "qaclick123")
-                .body(add)
-                .when().post("maps/api/place/add/json")
-                .then().assertThat().statusCode(200).extract().response();
+        RequestSpecification req =new RequestSpecBuilder()
+                .setBaseUri("https://rahulshettyacademy.com")
+                .addQueryParam("key", "qaclick123")
+                .setContentType(ContentType.JSON).build();
+
+        ResponseSpecification resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+
+        RequestSpecification res=given().spec(req)
+                .body(add);
+        Response response =res.when().post("/maps/api/place/add/json").
+                then().spec(resspec).extract().response();
         String r = response.asString();
 
     }
