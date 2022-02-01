cea1f95e6b8c9abf78fd27a63c9f99febc1bbc65
subhidh <subhidhagarwal@gmail.com>
2022-02-01 13:13:26 +0530

    deserialization using POJO

--- src/main/java/OAuth2/OAuth20.java
+++ src/main/java/OAuth2/OAuth20.java
@@ -2,13 +2,22 @@ package OAuth2;
 
 import static io.restassured.RestAssured.*;
 
+import POJO.Api;
+import POJO.GetCourse;
+import POJO.WebAutomation;
 import files.ReusableMethods;
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OAuth20 {
     public static void main(String[] args) throws InterruptedException {
@@ -25,7 +34,9 @@ public class OAuth20 {
         passwordKeys.sendKeys(Keys.ENTER);
         Thread.sleep(3000);*/
 
-        String url = "https://rahulshettyacademy.com/getCourse.php?state=asfddbf&code=4%2F0AX4XfWh2_EbgJ9CvdmiEaRC-l4bGmey4PtTUMN-gX0ogqF4py_E4evodHo8sfk8t0p67zg&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=5&prompt=consent#";
+        String[] courseTitles= { "Selenium Webdriver Java","Cypress","Protractor"};
+
+        String url = "https://rahulshettyacademy.com/getCourse.php?state=asfddbf&code=4%2F0AX4XfWjoPUfvCTuPh9VMODFRd0Obt19dTs-reZ3SPwKuByPFPOxAg-o701gJht5bbZRmZQ&scope=email+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=5&prompt=none#";
         String code = url.split("code=")[1].split("&scope")[0];
         System.out.println(code);
 
@@ -40,9 +51,37 @@ public class OAuth20 {
                 .asString();
         access_token = ReusableMethods.extractStringfromJson(access_token,"access_token");
 
-        String response = given().queryParam("access_token",access_token)
+        GetCourse response = given().queryParam("access_token",access_token)
+                .expect().defaultParser(Parser.JSON)
                 .when().get("https://rahulshettyacademy.com/getCourse.php")
-                .asString();
-        System.out.println(response);
+                .as(GetCourse.class);
+        System.out.println(response.getLinkedIn());
+        System.out.println(response.getCourses().getApi().get(0).getCourseTitle());
+
+        List<Api> apiCourse = response.getCourses().getApi();
+        for(int i=0; i<apiCourse.size();i++)
+        {
+            if(apiCourse.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+            {
+                apiCourse.get(i).getPrice();
+            }
+        }
+
+        //Get the course names of WebAutomation
+        ArrayList<String> a= new ArrayList<String>();
+
+
+        List<WebAutomation> w=response.getCourses().getWebAutomation();
+
+        for(int j=0;j<w.size();j++)
+        {
+            a.add(w.get(j).getCourseTitle());
+        }
+
+        List<String> expectedList=	Arrays.asList(courseTitles);
+
+        Assert.assertTrue(a.equals(expectedList));
+
+
     }
 }
